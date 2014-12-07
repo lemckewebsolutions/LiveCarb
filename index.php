a34345717b1ec740a55ab799a25c57f2de4d180f
Wouter Lemcke <wouterlemcke@gmail.com>
2014-12-07 11:43:57 +0100

    The if should be the other way around

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-if (strpos($_SERVER["HTTP_HOST"], "livecarb.nl") !== false)
-{
-	include_once("applications/Website/index.php");
-}
-elseif (strpos($_SERVER["HTTP_HOST"], "api.livecarb.nl") !== false)
+if (strpos($_SERVER["HTTP_HOST"], "api.livecarb.nl") !== false)
 {
 	include_once("applications/Api/index.php");
+}
+else
+{
+	include_once("applications/Website/index.php");
 }
